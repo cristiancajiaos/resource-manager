9c4c0d582c2f8efb88ecdf48d31c59db1d74ee41
cristiancajiaos <cristian.cajiao@alumnos.usm.cl>
2020-05-21 16:22:15 -0400

    Agregada navegación para el módulo de Cuenta

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -5,13 +5,20 @@ import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
+import { MaterialModule } from '../shared/modules/material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoadingModule } from '../shared/modules/loading/loading.module';
 
 
 @NgModule({
   declarations: [AccountComponent, ProfileComponent, SettingsComponent],
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoadingModule
   ]
 })
 export class AccountModule { }
