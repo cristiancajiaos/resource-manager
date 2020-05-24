60db07229ae34ec7d7b1d1e769599356082afef6
cristiancajiaos <cristian.cajiao@alumnos.usm.cl>
2020-05-24 18:14:26 -0400

    Vista de Ver Producto

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '../products.service';
+import { Location } from '@angular/common';
+import { Observable } from 'rxjs';
+import { ProductI } from 'src/app/shared/interfaces/product-i';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  product$: Observable<ProductI>;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productsSerivce: ProductsService,
+    private router: Router,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      if (params.has('id')) {
+        this.id = params.get('id');
+        this.product$ = this.productsSerivce.getProduct(this.id);
+      }
+    });
+  }
+
+  editProduct() {
+    this.router.navigate(['products', this.id, 'edit']);
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
